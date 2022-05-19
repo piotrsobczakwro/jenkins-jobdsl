47d501f0f5c0311471d9dd65c344210247ea4300
piotrsobczakwro <piotrsobczak.wro@hotmail.com>
2022-05-19 22:37:53 +0200

    new changes in pipeline

---
+++ pipeline-step-stage-retry-timeout.groovy
@@ -0,0 +1,29 @@
+pipeline {
+  //agent any
+  agent {
+    node {
+        // Declared label
+        label "master_node1"
+        //Declared custom workspace
+        customWorkspace "/home/helloworld"
+    }
+  }
+  stages {
+    stage("Build") {
+      steps {
+        retry(3){
+          script {
+          def name = "Peter"
+          // if statement in groovy
+          if(name == "Peter")
+            println("Hi,${name}")
+          else
+            println("You are not Peter")
+        }
+        sleep 10
+        echo "Hello world after 10secounds "
+        }
+      }
+    }
+  }
+}

7ee3e4749ecf9620b5e4d61d89ef77e55dadb6b6
piotrsobczakwro <piotrsobczak.wro@hotmail.com>
2022-05-19 22:28:07 +0200

    new changes in pipeline

---
+++ pipeline-step-stage.groovy
@@ -0,0 +1,28 @@
+pipeline {
+  //agent any
+  agent {
+    node {
+        // Declared label
+        label "master_node1"
+        //Declared custom workspace
+        customWorkspace "/home/helloworld"
+    }
+  }
+  stages {
+    stage("Build") {
+      steps {
+        script {
+          def name = "Peter"
+          // if statement in groovy
+          if name == "Peter"
+            println("Hi,${name}")
+          else
+            println("You are not Peter")
+          
+        sleep 2
+        echo "Hello world - end "
+        }
+      }
+    }
+  }
+}

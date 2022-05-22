8002a7f818c747feb2019bb3e92f672d69722b30
piotrsobczakwro <piotrsobczak.wro@hotmail.com>
2022-05-22 21:52:41 +0200

    New changes in repo

--- pipeline-stage-options-timestamps.groovy
+++ pipeline-stage-options-timestamps.groovy
@@ -2,15 +2,19 @@
 
 pipeline {
   agent any
+  options {
+    timestamps()
+  }
+
   stages {
     stage('Build') {
       tools {
         maven 'maven_3_8_5'
       }
-      options {
-        retry(3)
-        //timestamp option in stage 
-        timestamps()
+      // options {
+      //   retry(3)
+      //   //timestamp option in stage 
+      //   timestamps()
       }
       steps {
         echo "before"

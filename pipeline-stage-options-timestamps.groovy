5984c2dc5756f1ec078d9727b8944cd426867674
piotrsobczakwro <piotrsobczak.wro@hotmail.com>
2022-05-22 21:43:14 +0200

    New changes in repo

---
+++ pipeline-stage-options-timestamps.groovy
@@ -0,0 +1,31 @@
+// plugin timestamper
+
+pipeline {
+  agent any
+  
+  stages {
+    stage('Build') {
+      tools {
+        maven 'maven_3_8_5'
+      }
+      options {
+        retry(3)
+        //timestamp option in stage 
+        timestamps()
+      }
+      steps {
+        echo "before"
+        sh 'mvn --version'
+        error "After error"
+      }
+    }
+    stage('Build with failure') {
+      steps {
+        script {
+          // variable in jenkins 
+          /currentBuild.result = "FAILURE"
+        }
+      }
+    }
+  }
+}
